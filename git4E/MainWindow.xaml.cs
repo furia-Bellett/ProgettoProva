9c6dca79a0abf5537e2d4dc90b2a4bbc4582b3fe
Lorenzo Belletti <LORENZO.BELLETTI@local.ittcesena.it>
2022-10-13 08:25:43 +0200

    Versione base

---
+++ git4E/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace git4E
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        //Belletti Lorenzo 4^E
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            stampa_lbl.Content = "Hello word.com";
+
+
+            nome_lbl.Content = "Belletti Lorenzo 4^E";
+        }
+    }
+}
